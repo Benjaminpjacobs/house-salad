fa02845aa4a7c253cad167d378c99181bde37522
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-12 10:52:29 -0600

    refactors search presenter with memoization

--- app/presenters/search_presenter.rb
+++ app/presenters/search_presenter.rb
@@ -1,18 +1,23 @@
 class SearchPresenter
-  attr_reader :results
-
   def initialize(state)
-    @results = PropublicaService.search(state)[:results].map do |rep|
-        Representative.new(rep)
-      end
+    @results ||= initialize_representatives
+    @state = state
+  end
 
+  def initialize_representatives
+    PropublicaService.search(state)[:results].map do |rep|
+      Representative.new(rep)
+    end
   end
 
   def by_seniority
-    @results.sort_by{|rep| rep.seniority}.reverse
+    results.sort_by{|rep| rep.seniority}.reverse
   end
 
   def count
-    @results.count
+    results.count
   end
+  
+  private
+    attr_reader :results, :state
 end
