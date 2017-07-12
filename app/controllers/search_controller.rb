d119b6e9d5a9b3ed98e9c611b67c68f9733bc382
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-12 09:49:31 -0600

    creates search controller and adds faraday call

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,12 @@
+class SearchController < ApplicationController
+  def index
+    st = params[:state]
+    conn = Faraday.new(:url => 'https://api.propublica.org/congress/v1/')
+    results = conn.get do |req|
+      req.url "members/house/#{st}/current.json"
+      req.headers["X-API-Key"] = ENV["api-key"]
+    end
+    @member = JSON.parse(results.body, :symbolize_names => true)
+    binding.pry
+  end
+end
